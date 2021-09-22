bdf684026f677c4192dda130b5d37d91ad87f9c7
eggsnham07 <eggsnham07@gmail.com>
2021-09-22 12:01:40 -0600

    Added multi int support

--- pylib/mult.py
+++ pylib/mult.py
@@ -3,19 +3,13 @@ import sys
 import re
 
 count = 0
-into = 0
-intt = 0
+into = 1
 
 for arg in sys.argv:
     if not re.search("py", arg):
-        if count == 0:
-            into = float(int(arg))
-            count += 1
-        elif count == 1:
-            intt = float(int(arg))
-            count += 1
+        into *= float(int(arg))
 
-if isinstance(into, float) and isinstance(intt, float):
-   print(round(into * intt))
+if isinstance(into, float):
+   print(round(into))
 else:
     print("One of the arguments are invalid!")
