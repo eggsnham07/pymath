2320c6ac492b6af10f19aff5a68ec38d772b9e69
eggsnham07 <eggsnham07@gmail.com>
2021-09-23 15:07:31 -0600

    Fixed shell script

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ elif [ ! "$USER" == "root" ]; then
   if [ ! -d "$HOME/.local/bin" ]; then mkdir $HOME/.local/bin || echo "Making folder '$HOME/.local/bin' failed!"; fi
   if [ ! -d "$HOME/.local/bin/pymath" ]; then mkdir $HOME/.local/bin/pymath || echo "Making folder '$HOME/.local/bin/pymath' failed!"; fi
   if [ ! -d "$HOME/.local/bin/pymath/pylib" ]; then mkdir $HOME/.local/bin/pymath/pylib || echo "Making folder '$HOME/.local/bin/pymath/pylib' failed!"; fi
-  cp -r $PWD/* $HOME/.local/bin/pymath
+  cp -r $PWD/* $HOME/.local/bin/pymath || echo "Moving files to '$HOME/.local/bin/pymath' failed"
   echo "Done!"
 fi
